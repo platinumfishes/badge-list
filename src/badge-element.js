4062b08d4c2dae68d8675ebbfaed2321ce3cb3f1
Liz <elizabethblake@psu.edu>
2023-03-23 13:13:24 -0400

    made it look pretty

--- src/badge-element.js
+++ src/badge-element.js
@@ -1,24 +1,49 @@
 import { LitElement, html, css } from "lit";
+import "@lrnwebcomponents/simple-icon/simple-icon.js";
+import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
 
 class BadgeElement extends LitElement {
   static properties = {
     badgeImage: { type: String },
-    badgeTitle: {type: String},
+    badgeTitle: { type: String },
   };
 
   static styles = css`
+    :host {
+      display: block;
+      font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+      font-size: 48px;
+      color: black;
+      font-weight: 400;
+    }
     .badge-container {
-      border-bottom: 0;
+      border-width: 3px 3px 3px 48px;
+      border-style: solid;
+      border-color: #3e98d3;
+      border-radius: 12px;
       padding: 0 10px;
-      margin-bottom: 0;
+      margin: 24px;
       min-height: 41px;
       color: #fff;
-      background-color: #cfe6f4;
-      border-left: 15px solid #3e98d3;
+      background-color: #cfe5f3;
+    }
+    .badge-content-table {
+      display: table;
+    }
+    .badge-content {
+      display: table-row;
+      height: 100px;
+    }
+    .badge-icon {
+      display: table-cell;
+      padding: 24px;
+    }
+    .badge-title {
+      display: table-cell;
     }
     a {
       background-color: transparent;
-      color: #337ab7;
+      color: black;
       text-decoration: none;
       text-shadow: none;
     }
@@ -46,26 +71,31 @@ class BadgeElement extends LitElement {
       border: 0;
       vertical-align: middle;
     }
+
+    simple-icon {
+      --simple-icon-width: 120px;
+      --simple-icon-height: 120px;
+    }
   `;
 
   constructor() {
     super();
-    this.badgeTitle = 'Amazon Cognito';
-    this.badgeImage = 'https://badgesapp.psu.edu/uploads/badge/image/623/Cognito.png';
+    this.badgeTitle = "Amazon Cognito";
+    this.badgeIcon = "hardware:cast-connected"
   }
 
   render() {
     return html`
       <div class="badge-container">
-          <span class="badge-image">
-            <img
-              alt=""
-              src="${this.badgeImage}"
-            />
-          </span>
-            <span class="badge-title">
+        <div class="badge-content-table">
+          <div class="badge-content">
+            <div class="badge-icon">
+            <simple-icon accent-color="pink" icon="${this.badgeIcon}"></simple-icon>
+            </div>
+            <div class="badge-title">
               <a href="#"> ${this.badgeTitle} </a>
-            </span>
+            </div>
+          </div>
         </div>
       </div>
     `;
