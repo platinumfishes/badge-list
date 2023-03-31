7cebe201b04045ca04b8a19e68cdda2caedb1024
Liz <elizabethblake@psu.edu>
2023-03-31 18:01:40 -0400

    added stuff

--- src/badge-element.js
+++ src/badge-element.js
@@ -6,8 +6,9 @@ import "@lrnwebcomponents/a11y-collapse/lib/a11y-collapse-group.js";
 
 class BadgeElement extends LitElement {
   static properties = {
-    badgeImage: { type: String },
+    badgeIcon: { type: String },
     badgeTitle: { type: String },
+    badgeIconColor: { type: String },
   };
 
   static styles = css`
@@ -19,52 +20,14 @@ class BadgeElement extends LitElement {
       font-weight: 400;
     }
     .badge-container {
+      padding: 0px;
       border-width: 3px 3px 3px 48px;
       border-style: solid;
       border-color: #3e98d3;
       border-radius: 12px;
-      padding: 0 10px;
       margin: 24px;
-      min-height: 41px;
       color: #fff;
-      background-color: #cfe5f3;
-    }
-    .badge-icon {
-      display: table-cell;
-      padding: 24px;
-    }
-    .badge-title {
-      display: table-cell;
-    }
-    a {
-      background-color: transparent;
-      color: black;
-      text-decoration: none;
-      text-shadow: none;
-    }
-
-    a:active,
-    a:hover {
-      outline: 0;
-    }
-
-    a:hover {
-      color: #23527c;
-      text-decoration: underline;
-    }
-
-    a:hover,
-    a:active {
-      outline: 0;
-    }
-
-    a:hover {
-      cursor: pointer;
-    }
-
-    img {
-      border: 0;
-      vertical-align: middle;
+      background-color: white;
     }
 
     simple-icon {
@@ -72,27 +35,46 @@ class BadgeElement extends LitElement {
       --simple-icon-height: 120px;
     }
     a11y-collapse {
+      margin: 0px;
+      border-radius: 12px;
       --a11y-collapse-border: none;
       --a11y-collapse-heading-color: black;
+      --a11y-collapse-heading-background-color: #cfe5f3;
+      --a11y-collapse-vertical-padding: 0px;
+      --a11y-collapse-padding-top: 0px;
+      --a11y-collapse-padding-bottom: 0px;
+    }
+    p {
+      padding: 0px;
+      margin: 0px;
+    }
+
+    div {
+      padding: 0px;
+      margin: 0px;
     }
   `;
 
   constructor() {
     super();
-    this.badgeTitle = "Amazon Cognito";
-    this.badgeIcon = "hardware:cast-connected";
+    this.badgeTitle = " ";
+    this.badgeIcon = " ";
+    this.badgeIconColor = " ";
   }
 
   render() {
     return html`
       <div class="badge-container">
-        <a11y-collapse heading-button>
-          <p slot="heading"> 
-          <simple-icon accent-color="pink" icon="${this.badgeIcon}">
-          </simple-icon>${this.badgeTitle}</p>
+        <a11y-collapse>
+          <p slot="heading">
+            <simple-icon
+              accent-color="${this.badgeIconColor}"
+              icon="${this.badgeIcon}"
+            ></simple-icon>
+            ${this.badgeTitle}
+          </p>
           <div>random crap</div>
-          </a11y-collapse
-        >
+        </a11y-collapse>
       </div>
     `;
   }
