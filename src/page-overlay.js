ccb070245439aa569147eda1cd27142251699249
Platinumfish <mulerotmglel@gmail.com>
2023-04-06 12:25:21 -0400

    memes3

--- src/page-overlay.js
+++ src/page-overlay.js
@@ -2,8 +2,6 @@ import { LitElement, html, css } from "lit";
 import "@lrnwebcomponents/simple-icon/simple-icon.js";
 import "@lrnwebcomponents/simple-icon/lib/simple-icons.js";
 import "./search-bar";
-import "./badge-element";
-import "./badge-list";
 
 class PageOverlay extends LitElement {
     static properties = {
